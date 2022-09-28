e9036d1dca51916130a47ed292254b41dcf3ff4a
Lucas <lucas.robert@lepoles.org>
2022-09-28 16:46:10 +0200

    4eme push

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Post from '../post/Post';
 import Share from '../share/Share';
 import "./feed.css";
+import {Posts} from "../../dummyData";
 
 export default function Feed() {
   return (
     <div className="feed">Feed
     <div className="feedWrapper">
        <Share/>
-       <Post/>
+       {Posts.map( p=>(
+
+        <Post key={p.id} post={p}/>
+       ))}
+       
+       
     </div>
     </div>
   )
